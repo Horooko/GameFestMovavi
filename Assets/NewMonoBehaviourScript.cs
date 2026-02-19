5c57cd475d9203d0729d20183f225e1e0712bef1
Mac Pretty_Kotik <freeman.maybe@gmail.com>
2026-02-19 19:43:25 +0700

    Revert "fix"
    This reverts commit d6b787439c81bc45a7f8b6b5527eaa4ea04cbbad.

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
